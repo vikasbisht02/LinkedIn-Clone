0d7db38c80e2b6d8e28130322c26329698f0f26b
Vikas Bisht <vikasbisht732002@gmail.com>
2024-12-30 18:48:31 -0500

    Update file name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./components/layout/Layout";
+import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/auth/SignUpPage";
