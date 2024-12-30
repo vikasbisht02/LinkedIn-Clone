54cd69df715b08df7b1ab08a40144b936bbaebf5
Vikas Bisht <vikasbisht732002@gmail.com>
2024-12-30 12:37:43 -0500

    Fix import path in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./components/layout/Layout";
+import Layout from './components/layout/Layout';
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
